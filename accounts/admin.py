147616dc391636c395771baf60c24cb06aa21d68
Sandesh Nandagawe <sandagawe@gmail.com>
2023-10-16 18:28:19 +0530

    user registration page setup, navbar and footer

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
-from .models import User
+from .models import User, UserProfile
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active') #what to show on user form
     ordering = ('-date_joined',)
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
 
 
-
+# this will hide the password
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-# from django.db.models.fields.related import ForeignKey, OneToOneField
+from django.db.models.fields.related import ForeignKey, OneToOneField
 
 # from django.contrib.gis.db import models as gismodels
 # from django.contrib.gis.geos import Point
 
 # Create your models here.
+
+
 class UserManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
@@ -15,10 +17,10 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an username')
 
         user = self.model(
-            email = self.normalize_email(email),
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
         )
         user.set_password(password)
         user.save(using=self._db)
@@ -26,11 +28,11 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, first_name, last_name, username, email, password=None):
         user = self.create_user(
-            email = self.normalize_email(email),
-            username = username,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
         )
         user.is_admin = True
         user.is_active = True
@@ -53,7 +55,8 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=12, blank=True)
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICE, blank=True, null=True)
 
     # required fields
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -78,3 +81,27 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+
+class UserProfile(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(
+        upload_to='users/cover_photos', blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=6, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    location = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    # def full_address(self):
+    #     return f'{self.address_line_1}, {self.address_line_2}'
+
+    def __str__(self):
+        return self.user.email
